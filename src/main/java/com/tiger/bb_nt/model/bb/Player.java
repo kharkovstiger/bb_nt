b5ac81a3c9c30f4cf851a78ad65d99710655794b
Nick.Baru <nick.baru@ngsoft.com>
2018-11-18 12:33:06 +0200

    fix

--- src/main/java/com/tiger/bb_nt/model/bb/Player.java
+++ src/main/java/com/tiger/bb_nt/model/bb/Player.java
@@ -2,10 +2,12 @@ package com.tiger.bb_nt.model.bb;
 
 import com.tiger.bb_nt.util.SkillInitializer;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Map;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 @Document
 public class Player extends DefaultPlayer {
